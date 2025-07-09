32883e47a8e646b79de2f662b6c53e7d089e06c6
MomoKikuchi <kikumomo0311@icloud.com>
2025-07-09 19:30:48 +0900

    k

--- Pokebell/CustomView.swift
+++ Pokebell/CustomView.swift
@@ -21,105 +21,14 @@ struct CustomView: View {
                 .font(.custom("x8y12pxTheStrongGamer", size: 30))
                 .foregroundColor(Color("blackgray"))
                 .padding(.top,100)
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 25)
-//                    .fill(Color(selectedColor))
-//                    .overlay {
-//                        RoundedRectangle(cornerRadius: 20)
-//                            .fill(Color("blackgray"))
-//                            .overlay {
-//                                RoundedRectangle(cornerRadius: 15)
-//                                    .fill(Color("pokegreen"))
-//                                    .overlay {
-//                                        VStack {
-//                                            HStack {
-//                                                Image(systemName: "antenna.radiowaves.left.and.right")
-//                                                Image(systemName: "bell.fill")
-//                                                Spacer()
-//                                                Image(systemName: "speaker.wave.3.fill")
-//                                                Image(systemName: "music.note")
-//                                                
-//                                            }
-//                                            .font(.system(size: 15))
-//                                            .foregroundColor(Color("blackgray"))
-//                                            .padding(.horizontal, 15)
-//                                            HStack {
-//                                                //                                    Text(entry.message)
-//                                                Text("sample")
-//                                                //                                    Text(entry.sender)
-//                                                
-//                                            }
-//                                            .foregroundStyle(Color("blackgray"))
-//                                            .font(.custom("x8y12pxTheStrongGamer", size: 20))
-//                                            .padding([.horizontal, .bottom], 4)
-//                                            
-//                                        }
-//                                    }
-//                                    .padding(10)
-//                            }
-//                            .padding(.horizontal, 10)
-//                            .padding(.top,10)
-//                            .padding(.bottom, 60)
-//                    }
-//                    .padding(.horizontal, 30)
-//                    .padding(.top, 30)
-//                    .padding(.bottom,20)
-//                
-//                
-//            }
-            ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color(selectedColor))
-                    .shadow(color: .gray.opacity(0.4), radius: 10, x: 0, y: 5)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Color("blackgray"))
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 15)
-                                    .fill(Color("pokegreen"))
-                                    .overlay {
-                                        VStack {
-                                            HStack {
-                                                Image(systemName: "antenna.radiowaves.left.and.right")
-                                                Image(systemName: "bell.fill")
-                                                Spacer()
-                                                Image(systemName: "speaker.wave.3.fill")
-                                                Image(systemName: "music.note")
-                                                
-                                            }
-                                            .font(.system(size: 15))
-                                            .foregroundColor(Color("blackgray"))
-                                            .padding(.horizontal, 15)
-                                            HStack {
-                                                //                                    Text(entry.message)
-                                                Text("sample")
-                                                //                                    Text(entry.sender)
-                                                
-                                            }
-                                            .foregroundStyle(Color("blackgray"))
-                                            .font(.custom("x8y12pxTheStrongGamer", size: 20))
-                                            .padding([.horizontal, .bottom], 4)
-                                            
-                                        }
-                                    }
-                                    .padding(10)
-                            }
-                            .padding(.horizontal, 10)
-                            .padding(.top,10)
-                            .padding(.bottom, 60)
-                    }
-                    .padding(.top, 30)
-                    
-                
-                
-            }
-            .frame(width: UIScreen.main.bounds.width * 0.85)
-            .aspectRatio(20/13, contentMode: .fit)
+                .padding(.bottom,30)
+            PokeSelectedColor(color: Color(selectedColor))
+                .padding(20)
             
             
             Spacer()
             HStack(spacing: 16) {
-                ForEach(["pink3", "lightgray", "widgetblue"], id: \.self) { colorName in
+                ForEach(["pink3", "pokegray", "widgetblue","widgetpurple","widgetblack"], id: \.self) { colorName in
                     Button(action: {
                         withAnimation {
                             selectedColor = colorName
@@ -131,7 +40,7 @@ struct CustomView: View {
                             Color(colorName)
                                 .frame(width: 40, height: 40)
                                 .cornerRadius(20)
-                                
+                            
                             
                             // 選択されているときに二重の青いリングを表示
                             if selectedColor == colorName {
@@ -147,14 +56,80 @@ struct CustomView: View {
                     }
                 }
             }
-            .padding(.bottom, 330)
+            .padding(.bottom,330)
+           
             
         }
     }
-        
-        private func refreshWidget() {
-            WidgetCenter.shared.reloadAllTimelines()
-        }
+    
+    private func refreshWidget() {
+        WidgetCenter.shared.reloadAllTimelines()
     }
+}
 
+struct PokeSelectedColor: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(color)
+            .shadow(color: .gray.opacity(0.4), radius: 10, x: 0, y: 5)
+            .overlay {
+                VStack {
+                    PokeGreenBorder()
+                    Spacer()
+                }
+            }
+            .frame(height: 190)
+    }
+}
 
+struct PokeGreenBorder: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color("blackgray"))
+            .overlay {
+                PokeGreenPart()
+            }
+            .padding(.horizontal, 10)
+            .padding(.top,10)
+            .frame(height: 110)
+    }
+}
+
+struct PokeGreenPart: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(Color("pokegreen"))
+            .overlay {
+                VStack {
+                    HStack {
+                        Image(systemName: "antenna.radiowaves.left.and.right")
+                        Image(systemName: "bell.fill")
+                        Spacer()
+                        Image(systemName: "speaker.wave.3.fill")
+                        Image(systemName: "music.note")
+                        
+                    }
+                    .font(.system(size: 15))
+                    .foregroundColor(Color("blackgray"))
+                    .padding(.horizontal, 15)
+                    HStack {
+                        //                                    Text(entry.message)
+                        Text("sample")
+                        //                                    Text(entry.sender)
+                        
+                    }
+                    .foregroundStyle(Color("blackgray"))
+                    .font(.custom("x8y12pxTheStrongGamer", size: 20))
+                    .padding([.horizontal, .bottom], 4)
+                    
+                }
+            }
+            .padding(10)
+            .frame(height: 100)
+    }
+}
+
+#Preview {
+    CustomView()
+}
